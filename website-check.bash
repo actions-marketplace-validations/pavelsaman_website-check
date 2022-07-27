59296f4637bb5247f8d90f23bcf47c7cea9d4ade
Pavel Saman <samanpavel@gmail.com>
2022-07-27 12:47:17 +0200

    fix: do not redirect
    Remove --location that redirects on 3xx status codes.

--- website-check.bash
+++ website-check.bash
@@ -6,7 +6,7 @@ expected_status_code=$2
 [ -z "$expected_status_code" ] && expected_status_code=200
 [ -z "$1" ] && { echo "No URL, exiting."; exit 1; }
 
-status_code=$(curl --silent --location --output /dev/null --write-out "%{http_code}" "$url")
+status_code=$(curl --silent --output /dev/null --write-out "%{http_code}" "$url")
 [ "$status_code" != "$expected_status_code" ] && { echo "$status_code"; exit 1; }
 
 exit 0
